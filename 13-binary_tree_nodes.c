dbd3567c5e594af261e1b6be47efb10402fa1872
root <lanremidelola@gmail.com>
2023-08-02 00:03:40 -0700

    alx

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,20 +1,18 @@
 #include "binary_trees.h"
+
 /**
  * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
  *
- * @tree: A pointer to the root node of the tree to count the number of nodes.
+ * @tree: node.
  *
  * Return: 0 if tree is NULL.
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	size_t l_left, r_right;
-
-	if (tree == NULL || binary_tree_is_leaf(tree) == 1)
+	if (tree == NULL)
 		return (0);
-
-	l_left = binary_tree_nodes(tree->left);
-	r_right = binary_tree_nodes(tree->right);
-
-	return (r_right + l_left + 1);
+	if (tree->right != NULL || tree->left != NULL)
+		return  (1 + binary_tree_nodes(tree->right) +
+			 binary_tree_nodes(tree->left));
+	return (0);
 }
